7072989b966626a7a2d5d0bb7c70f9d2a5b710cc
tushal kumar <tushalkashyap11@gmail.com>
2021-06-07 23:59:54 -0700

    Added:create the binary tree shown by given figure

--- src/test/java/com/binarysearchtree/BinarySearchTreeTest.java
+++ src/test/java/com/binarysearchtree/BinarySearchTreeTest.java
@@ -3,10 +3,10 @@ package com.binarysearchtree;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class BinarySearchTreeTest {
+public class BinarySearchTreeTest<I extends Number> {
     @Test
     public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree() {
-        BinarySearchTreeTest myBinaryTree = new BinarySearchTreeTest();
+        BinarySearchTreeTest<Integer> myBinaryTree = new BinarySearchTreeTest<>();
         myBinaryTree.add(56);
         myBinaryTree.add(30);
         myBinaryTree.add(70);
@@ -18,6 +18,25 @@ public class BinarySearchTreeTest {
     }
 
     private void add(int i) {
+    }
+    @Test
+    public void givenNumbersWhenAddedToBinaryTreeShouldReturnSizeThirteen() {
+        BinarySearchTree<Integer> myBinaryTree = new BinarySearchTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(11);
+        myBinaryTree.add(65);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        int size = myBinaryTree.getSize();
+        Assertions.assertEquals(13, size);
     }
 
 
